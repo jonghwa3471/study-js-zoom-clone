8d45a6a445fa8b5c509dd541261b8700292fbb3e
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-10-01 19:54:30 +0900

    #2.8 Room Count part One

--- src/server.js
+++ src/server.js
@@ -17,6 +17,21 @@ const handleListen = () =>
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
   socket["nickname"] = "Anon";
   socket.onAny((event) => {
