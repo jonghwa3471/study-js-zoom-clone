c8b3702f987c350916ff8518403fa93596e39802
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-10-07 23:25:48 +0900

    #3.4 Rooms

--- src/server.js
+++ src/server.js
@@ -14,6 +14,14 @@ app.get("/*", (_, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+});
+
 const handleListen = () =>
   console.log(`✅ Listening on http://localhost:5001 🚀`);
 
